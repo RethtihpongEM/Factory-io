2a2fca0e916bc788bb7c2d3ed0d45cc0ed4acc59
sunchhay395 <sunchhay395@gmail.com>
2023-07-15 13:40:12 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+// import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 // import React from "react";
 export default defineConfig({
     plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
-        reactRefresh(),
+        // reactRefresh(),
+      react()
     ],
 });
